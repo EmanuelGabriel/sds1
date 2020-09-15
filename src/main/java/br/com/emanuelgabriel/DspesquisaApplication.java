5d385e6c035806955dcc5ef98575aa3b267bdee1
Samuel Gabriel <emanuel.gabriel.sousa@protonmail.com>
2020-09-15 11:37:26 -0300

    first commit

---
+++ src/main/java/br/com/emanuelgabriel/DspesquisaApplication.java
@@ -0,0 +1,13 @@
+package br.com.emanuelgabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspesquisaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emanuelgabriel/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emanuelgabriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
